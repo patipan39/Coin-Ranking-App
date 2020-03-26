b49f22ef4326b58e0b12a7582f150e26da85d684
Patipan Injai <i.patipan2539@gmail.com>
2020-03-26 23:11:25 +0700

    [CRK-2] create list item for submit new item in list adapter

--- app/src/main/java/com/patipan/dev/coinrankingapp/CoinRankingViewModel.kt
+++ app/src/main/java/com/patipan/dev/coinrankingapp/CoinRankingViewModel.kt
@@ -1,7 +1,21 @@
 package com.patipan.dev.coinrankingapp
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.patipan.dev.coinrankingapp.adapter.BaseCoinRankingAdapterData
+import com.patipan.dev.coinrankingapp.adapter.CoinRankingLeftData
+import com.patipan.dev.coinrankingapp.adapter.CoinRankingRightData
 
 class CoinRankingViewModel : ViewModel() {
+    private val listBaseCoinItemAdapter = arrayListOf<BaseCoinRankingAdapterData>()
+    private val mutableCoinLiveData = MutableLiveData<ArrayList<BaseCoinRankingAdapterData>>()
 
+    fun setCoinRankingItemList() {
+        listBaseCoinItemAdapter.add(CoinRankingLeftData(2))
+        listBaseCoinItemAdapter.add(CoinRankingRightData(1))
+
+        mutableCoinLiveData.value = listBaseCoinItemAdapter
+    }
+
+    fun observeMutableCoinItemList() = mutableCoinLiveData
 }
