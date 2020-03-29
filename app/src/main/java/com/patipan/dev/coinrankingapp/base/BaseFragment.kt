3db1904f623b84b6453a5a12abbba910c9fff37f
Patipan Injai <i.patipan2539@gmail.com>
2020-03-29 23:00:50 +0700

    [CRK-4] handle show hide keyboard and refactor code

--- app/src/main/java/com/patipan/dev/coinrankingapp/base/BaseFragment.kt
+++ app/src/main/java/com/patipan/dev/coinrankingapp/base/BaseFragment.kt
@@ -1,13 +1,14 @@
 package com.patipan.dev.coinrankingapp.base
 
 import android.content.Context
+import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.fragment.app.Fragment
 
 abstract class BaseFragment : Fragment() {
 
-    fun AppCompatEditText.hideKeyBoard() {
+    fun View.hideKeyBoard() {
         this.clearFocus()
         val inputMethodManger =
             context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
